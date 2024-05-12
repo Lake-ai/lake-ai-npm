0bcdefc34e0c9414e8ed173cf3a1d79ba65da586
unknown <apurvjha123@gmail.com>
2024-05-12 10:45:00 +0530

    npm package

---
+++ dist/ChatBot.d.ts
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ChatBot.css";
+interface ChatBotProps {
+    initialMessage?: string;
+    chatbotTitle?: string;
+    LLM: string;
+    brandImage?: string;
+    apiKey?: string;
+    switchAPI?: boolean;
+    suggestions?: string[];
+    rateLimitDuration?: number;
+}
+declare const ChatBot: React.FC<ChatBotProps>;
+export default ChatBot;
